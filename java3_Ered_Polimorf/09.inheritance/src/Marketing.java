e59cf032a223e123564916b8d0d247102d614138
riccardo fusco <rriccardofusco@gmail.com>
2023-04-26 11:07:59 +0200

    java3 non va

--- java3_Ered_Polimorf/09.inheritance/src/Marketing.java
+++
@@ -1,8 +0,0 @@
-public class Marketing extends Employer {
-    public Marketing(String name, String code, int salary){
-        super(name, code, salary);
-    }
-
-    public String stampaSalario() { return "ciao"; };
-
-}
